bcf099f4e5cf6a4ba2ecfb29af27a6d58adc7895
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-09-03 21:35:44 -0300

    Create employee entity

---
+++ src/main/java/com/pablopetr/restaurant_api/modules/employees/entities/Employee.java
@@ -0,0 +1,31 @@
+package com.pablopetr.restaurant_api.modules.employees.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@Entity(name="employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private String role;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
