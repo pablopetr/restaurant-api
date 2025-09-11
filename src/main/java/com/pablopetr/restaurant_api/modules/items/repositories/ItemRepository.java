6ba2369bfdaf0620b52a0b449cb2358a545632e7
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-09-10 21:58:20 -0300

    Create ItemRepository

---
+++ src/main/java/com/pablopetr/restaurant_api/modules/items/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.pablopetr.restaurant_api.modules.items.repositories;
+
+import com.pablopetr.restaurant_api.modules.items.entities.ItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ItemRepository extends JpaRepository<ItemEntity, UUID> {
+}
